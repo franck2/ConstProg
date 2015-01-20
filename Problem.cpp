f4af57c37cc2fe87dd528fd1d0510beda2df6a24
JulienOuvrard <julien.ouvrard311@gmail.com>
2015-01-20 17:49:24 +0000

    ajout problem

---
+++ Problem.cpp
@@ -0,0 +1,18 @@
+#include "Problem.hpp"
+
+using namespace std;
+
+Problem::Problem(){
+}
+
+Problem::Problem(int n, vector<Constraint> cst){
+
+}
+
+Noeud Problem::initialNode(){
+
+}
+
+Proof Problem::testSat(Noeud node){
+
+}
---
+++ Problem.hpp
@@ -0,0 +1,20 @@
+#ifndef DEF_PROBLEM
+#define DEF_PROBLEM
+
+#include <iostream>
+#include <vector>
+enum Proof {echec, succes, indefini};
+class Problem{
+
+public:
+	Problem();
+	Problem(int n, std::vector<Constraint> cst);
+	Noeud initialNode();
+	Proof testSat(Noeud node);
+
+private:
+	int n;
+	std::vector<Constraint> constraints;
+};
+
+#endif
