7b1e07e18dfc446347da86701f27ff4292b69fd7
f.boncler@gmail.com <3UGGAGdP>
2015-01-20 17:44:13 +0000

    ajout prob

---
+++ backtracking.hpp
@@ -0,0 +1,17 @@
+#ifndef DEF_BACKTRACKING
+#define DEF_BACKTRACKING
+
+#include <iostream>
+#include <set>
+#include <vector>
+
+class Backtracking{
+
+public:
+	Backtracking();
+	int solve();
+	void branch(Noeud noeud);
+
+};
+
+#endif
