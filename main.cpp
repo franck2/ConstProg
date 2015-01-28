f7655a7ba90204138d9bf53e6d39e1d80d8ffab7
franck2 <f.boncler@gmail.com>
2015-01-28 16:40:00 +0100

    main amélioré

--- main.cpp
+++ main.cpp
@@ -6,20 +6,58 @@
 #include "Nqueen_colonne.hpp"
 #include "Nqueen_diago.hpp"
 #include "BranchAndPrune.hpp"
+#include "Solv.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
 
-	Nqueen_colonne q;
-	Nqueen_diago q2;
-	std::vector<Constraint*> tab;
-	//tab.push_back(&q);
-	tab.push_back(&q2);
-	//BacktrackingNonRec b(0, tab);*/
-	
-	BranchAndPrune bb(11,tab);
-	int nb_sol = bb.solve();
-	cout<<"il y a "<<nb_sol<<" solutions"<<endl;
+	bool arreter = false;
+
+	while(!arreter){
+		int choix = 0;
+
+		cout<<"Quel algo voulez vous utiliser ?"<<endl;
+		cout<<"0- Arreter"<<endl;
+		cout<<"1- backtrackin non recursif"<<endl;
+		cout<<"2- branch and prune non recursif"<<endl;
+
+		cin>>choix;
+		if(choix > 0 && choix < 3){
+			int nb_dame = 0;
+			
+			cout<<"Combien de dames utiliser ?"<<endl;
+			cin>>nb_dame;
+
+			if(nb_dame>=0){
+				std::vector<Constraint*> contraintes;
+				Solveur *solv;
+				if(choix == 1){
+					Nqueen_colonne contrainte_colonne;
+					Nqueen_diago contrainte_diago;
+					contraintes.push_back(&contrainte_colonne);
+					contraintes.push_back(&contrainte_diago);
+					solv = new BacktrackingNonRec(nb_dame, contraintes);
+				}
+				else if(choix == 2){
+					Nqueen_diago contrainte_diago;
+					contraintes.push_back(&contrainte_diago);
+					solv = new BranchAndPrune(nb_dame, contraintes);
+				}
+
+				int nb_sol = solv->solve();
+				cout<<"il y a "<<nb_sol<<" solutions"<<endl;
+			}	
+			else{
+				cout<<"Ca ne va pas être possible, recommencer !"<<endl;
+			}
+		}
+		else if(choix == 0){
+			arreter = true;
+		}
+		else{
+			cout<<"Ce choix n'existe pas !"<<endl;
+		}
+	}
 	return 0;
 }
